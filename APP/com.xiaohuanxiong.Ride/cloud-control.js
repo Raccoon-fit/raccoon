34f0d794328f38bf6cf150aaa249ae342c5fabb8
Raccoon-fit <2051775505@qq.com>
2026-04-06 17:51:08 +0800

    更新 cloud-control.js

--- APP/com.xiaohuanxiong.Ride/cloud-control.js
+++ APP/com.xiaohuanxiong.Ride/cloud-control.js
@@ -1,5 +1,5 @@
 // 云控变量
-var CLOUD_STATUS = 0;
+var CLOUD_STATUS = 4;
 // 0=无, 1=普通, 2=重要, 4=维护公告
 
 // 普通公告（状态1）
@@ -11,4 +11,4 @@ var CLOUD_NOTICE_IMPORTANT = "开服公告";
 // 维护公告（状态4）
 var CLOUD_MAINTENANCE_TITLE = "系统维护通知";
 var CLOUD_MAINTENANCE_CONTENT = "云端服务正在升级维护，期间部分功能暂不可用，本地编辑不受影响。<br>bug反馈社区808417324";
-var CLOUD_MAINTENANCE_TIME = "2026年4月4日 20:30 - 21:00";
+var CLOUD_MAINTENANCE_TIME = "2026年4月6日 18:00 - 20:00";
